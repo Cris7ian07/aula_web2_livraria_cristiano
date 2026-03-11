3026c9ee6cef93cd297ae5668bfe3f62daf31bb9
Cris7ian07 <giannini_cris_anjo@hotmail.com>
2026-03-10 22:43:56 -0300

    feature/controller listar autores adicionado

---
+++ src/modules/autores/autores.controller.ts
@@ -0,0 +1,16 @@
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import { AutoresService } from './autores.service';
+
+@Controller('autores')
+export class AutoresController {
+  constructor(private readonly autoresService: AutoresService) {}
+
+  @Get('/listar-autores')
+  listaAutores() {
+    return this.autoresService.listarAutores();
+  }
+  @Get('/listar-autor/:id')
+  listarAutor(@Param('id', ParseIntPipe) id: number) {
+    return this.autoresService.listarAutor(id);
+  }
+}
