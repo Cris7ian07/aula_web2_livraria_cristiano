973bd260f6e25040486477adc5f279954503aafa
Cris7ian07 <giannini_cris_anjo@hotmail.com>
2026-03-26 22:44:56 -0300

    feat/ novo endpoint atualizar autor add

--- src/modules/autores/autores.controller.ts
+++ src/modules/autores/autores.controller.ts
@@ -1,6 +1,15 @@
-import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Put,
+} from '@nestjs/common';
 import { AutoresService } from './autores.service';
-
+import { Body } from '@nestjs/common';
+import { Post } from '@nestjs/common';
+import { CriarAutorDto } from './autores.dto';
 @Controller('autores')
 export class AutoresController {
   constructor(private readonly autoresService: AutoresService) {}
@@ -12,5 +21,20 @@ export class AutoresController {
   @Get('/listar-autor/:id')
   listarAutor(@Param('id', ParseIntPipe) id: number) {
     return this.autoresService.listarAutor(id);
+  }
+  @Post('/criar-autor')
+  criarAutor(@Body() bodyRequest: CriarAutorDto) {
+    return this.autoresService.criarAutor(bodyRequest);
+  }
+  @Put('/atualizar-autor/:id')
+  atualizarautor(
+    @Param('id', ParseIntPipe) idAutor: number,
+    @Body() bodyRequest: any,
+  ) {
+    return this.autoresService.atualizarAutor(idAutor, bodyRequest);
+  }
+  @Delete('/deletar-autor/:id')
+  deletarAutor(@Param('id', ParseIntPipe) idAutor: number) {
+    return this.autoresService.deletarAutor(idAutor);
   }
 }
--- src/modules/autores/autores.service.ts
+++ src/modules/autores/autores.service.ts
@@ -1,4 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { AtualizarAutorDto, CriarAutorDto } from './autores.dto';
 let autores = [
   {
     id: 1,
@@ -19,13 +24,50 @@ let autores = [
 @Injectable()
 export class AutoresService {
   listarAutores() {
+    if (!autores) {
+      return 'Autor não encontrado';
+    }
     return autores;
   }
+
   listarAutor(id: number) {
     const autorEncontrado = autores.find((autor) => autor.id === id);
     if (!autorEncontrado) {
-      return 'Autor não encontrado';
+      throw new NotFoundException('Autor não encontrado');
     }
     return autorEncontrado;
+  }
+  criarAutor(bodyRequest: CriarAutorDto) {
+    if (!bodyRequest.nome || !bodyRequest.email) {
+      return 'Nome e email são obrigatórios';
+    }
+    autores.push({
+      id: autores.length + 1,
+      nome: bodyRequest.nome,
+      email: bodyRequest.email,
+    });
+    return autores;
+  }
+  atualizarAutor(idAutor: number, bodyRequest: AtualizarAutorDto) {
+    const autorEncontrado = this.listarAutor(idAutor);
+
+    if (!bodyRequest.nome && !bodyRequest.email) {
+      throw new BadRequestException('Nome e email são obrigatórios');
+    }
+    if (bodyRequest.nome) {
+      autorEncontrado.nome = bodyRequest.nome;
+    }
+    if (bodyRequest.email) {
+      autorEncontrado.email = bodyRequest.email;
+    }
+    return autorEncontrado;
+  }
+
+  deletarAutor(idAutor: number) {
+    this.listarAutor(idAutor);
+
+    autores = autores.filter((autor) => autor.id !== idAutor);
+
+    return autores;
   }
 }
