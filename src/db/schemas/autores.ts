689145449b4cedf608bf1c7a235e3821f1df3ad1
Cris7ian07 <giannini_cris_anjo@hotmail.com>
2026-04-30 22:42:57 -0300

    feat/ nova coluna ativo adicionada na tabela autores

--- src/db/schemas/autores.ts
+++ src/db/schemas/autores.ts
@@ -1,9 +1,16 @@
-import { datetime, mssqlTable, int, varchar } from 'drizzle-orm/mssql-core';
+import {
+  datetime,
+  mssqlTable,
+  int,
+  varchar,
+  bit,
+} from 'drizzle-orm/mssql-core';
 
 export const autoresTabela = mssqlTable('autores', {
   id: int('id').primaryKey().identity(),
   nome: varchar('nome', { length: 100 }).notNull(),
   email: varchar('email', { length: 255 }).notNull().unique(),
+  ativo: bit('ativo').notNull().default(true),
   criadoEm: datetime('criado_em').notNull().defaultGetDate(),
 });
 export type Autor = typeof autoresTabela.$inferSelect;
--- src/modules/autores/autores.service.ts
+++ src/modules/autores/autores.service.ts
@@ -5,21 +5,22 @@ import {
 } from '@nestjs/common';
 import { AtualizarAutorDto, CriarAutorDto } from './autores.dto';
 import { AutoresRepository } from './autores.repository';
+
 let autores = [
   {
     id: 1,
     nome: 'João da Silva',
-    email: 'joao.silva@gmail.com',
+    email: 'joao@gmail.com',
   },
   {
     id: 2,
     nome: 'Maria Oliveira',
-    email: 'maria.oliveira@gmail.com',
+    email: 'maria@gmail.com',
   },
   {
     id: 3,
     nome: 'Pedro Santos',
-    email: 'pedro.santos@gmail.com',
+    email: 'pedro@gmail.com',
   },
 ];
 @Injectable()
@@ -27,40 +28,27 @@ export class AutoresService {
   constructor(private readonly autoresRepository: AutoresRepository) {}
 
   async listarAutores() {
-    return await this.autoresRepository.listarAutores();
+    return await this.autoresRepository.listarAutores;
   }
 
-  listarAutor(id: number) {
-    const autorEncontrado = autores.find((autor) => autor.id === id);
+  async listarAutor(id: number) {
+    const autorEncontrado = await this.autoresRepository.listarAutor(id);
+
     if (!autorEncontrado) {
-      throw new NotFoundException('Autor não encontrado');
+      throw new NotFoundException(`Autor com o id ${id} não encontrado`);
     }
-    return autorEncontrado;
-  }
-  criarAutor(bodyRequest: CriarAutorDto) {
-    if (!bodyRequest.nome || !bodyRequest.email) {
-      return 'Nome e email são obrigatórios';
-    }
-    autores.push({
-      id: autores.length + 1,
-      nome: bodyRequest.nome,
-      email: bodyRequest.email,
-    });
-    return autores;
-  }
-  atualizarAutor(idAutor: number, bodyRequest: AtualizarAutorDto) {
-    const autorEncontrado = this.listarAutor(idAutor);
 
-    if (!bodyRequest.nome && !bodyRequest.email) {
-      throw new BadRequestException('Nome e email são obrigatórios');
-    }
-    if (bodyRequest.nome) {
-      autorEncontrado.nome = bodyRequest.nome;
-    }
-    if (bodyRequest.email) {
-      autorEncontrado.email = bodyRequest.email;
-    }
     return autorEncontrado;
+  }
+
+  criarAutor(bodyRequest: CriarAutorDto) {
+    return this.autoresRepository.criarAutor(bodyRequest);
+  }
+
+  async atualizarAutor(idAutor: number, bodyRequest: AtualizarAutorDto) {
+    await this.listarAutor(idAutor);
+
+    return this.autoresRepository.atualizarAutor(idAutor, bodyRequest);
   }
 
   deletarAutor(idAutor: number) {
