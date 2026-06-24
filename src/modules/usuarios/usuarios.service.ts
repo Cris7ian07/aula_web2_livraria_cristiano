50fea5d7df43ac1d556e9d8f3f8d5054fd23662f
Cris7ian07 <giannini_cris_anjo@hotmail.com>
2026-06-23 22:48:48 -0300

    feat/ criado criarusuario

--- src/modules/usuarios/usuarios.service.ts
+++ src/modules/usuarios/usuarios.service.ts
@@ -1,6 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+import { UsuariosRepository } from './usuarios.repository';
+import { CriarUsuarioDto } from './usuarios.dto';
 
 @Injectable()
 export class UsuariosService {
-  constructor() {}
+  constructor(private readonly usuariosRepository: UsuariosRepository) {}
+
+  async buscarUsuarioPorEmail(email: string) {
+    return await this.usuariosRepository.buscarUsuarioPorEmail(email);
+  }
+
+  async criarUsuario(usuario: CriarUsuarioDto) {
+    const { email, nome, password } = usuario;
+
+    const usuarioEncontrado = await this.buscarUsuarioPorEmail(email);
+
+    if (usuarioEncontrado) {
+      throw new ConflictException('Usuário já cadastrado com este email');
+    }
+
+    const passwordHashed = await bcrypt.hash(password, 10);
+
+    return await this.usuariosRepository.criarUsuario({
+      nome: nome,
+      email: email,
+      password: passwordHashed,
+    });
+  }
 }
